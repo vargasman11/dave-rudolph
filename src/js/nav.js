0dfeb0d3b81fdccf38f62eeaa10a0280476759a5
Jerry Vargas <vargasman11@gmail.com>
2025-06-19 08:15:17 -0500

    implementing functioning mobile nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -3,13 +3,22 @@ function mobileNav() {
   const mobileNav = document.getElementById('mobileNav');
   const icon = document.querySelector('.icon-toggle use');
 
+  mobileNavIcon.addEventListener('click', function () {
+    const isOpen = mobileNav.classList.contains('w-full');
 
-  mobileNavIcon.addEventListener('click', function() {
-    mobileNav.classList.toggle('hidden')
+    if (isOpen) {
+      // Closing: shrink width to 0
+      mobileNav.classList.remove('w-full');
+      mobileNav.classList.add('w-0');
+    } else {
+      // Opening: expand width to full
+      mobileNav.classList.remove('w-0');
+      mobileNav.classList.add('w-full');
+    }
 
     const isBars = icon.getAttribute('xlink:href') === '#bars';
     icon.setAttribute('xlink:href', isBars ? '#xmark' : '#bars');
-  })
+  });
 }
 
 export default mobileNav();
