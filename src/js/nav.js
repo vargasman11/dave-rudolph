8c3cc5b336adc05d7bfa80f383b4c11a7a522ed0
Jerry Vargas <vargasman11@gmail.com>
2025-09-24 14:33:15 -0500

    adding dark mode JS removing commented out code

--- src/js/nav.js
+++ src/js/nav.js
@@ -40,6 +40,19 @@ function mobileNav() {
     });
   });
 
+  // Dark Mode
+  const toggleBtn = document.getElementById('darkModeToggle');
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Update button text
+    if (document.body.classList.contains('dark-mode')) {
+      toggleBtn.textContent = "☀️ Light Mode";
+    } else {
+      toggleBtn.textContent = "🌙 Dark Mode";
+    }
+  });
+  
 }
 
 export default mobileNav();
