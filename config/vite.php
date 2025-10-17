e42e03829067328d730b50f135f6d364423494b8
Jerry Vargas <>
2025-10-17 00:19:23 -0400

    Fix Vite manifest path

--- config/vite.php
+++ config/vite.php
@@ -4,7 +4,7 @@ use craft\helpers\App;
 
 return [
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
-    'manifestPath' => '@webroot/dist/manifest.json',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
     'devServerPublic' => 'https://daverudolphmusic.ddev.site:5173',
     'serverPublic' => App::env('PRIMARY_SITE_URL') . '/dist/',
     'errorEntry' => '',
